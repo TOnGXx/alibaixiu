e01e6052746e9a3c6889d5d02e5c12953790c6d0
tong <550601093@qq.com>
2019-12-08 16:05:33 +0800

    用户登录信息显示用户信息+

--- routes/actions/other/loginStatus.js
+++ routes/actions/other/loginStatus.js
@@ -1,7 +1,9 @@
 module.exports = async (req, res) => {
 	if (req.session && req.session.userInfo) {
-		res.send('var isLogin = true')
+		const s = `var isLogin = true; var userId=\"${req.session.userInfo._id}\"`
+		res.send(s)
 	}else {
 		res.send('var isLogin = false')
 	}
 };
+ 
